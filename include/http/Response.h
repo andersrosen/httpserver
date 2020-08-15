7b32d8d8b1cd85661983ecc839b6d1928a48f425
Anders Rosén <panrosen@gmail.com>
2020-08-15 16:03:00 +0200

    Implemented constants for all official HTTP response status codes

--- include/http/Response.h
+++ include/http/Response.h
@@ -18,11 +18,73 @@ namespace ARo::Http {
             uint16_t code;
             std::string description;
 
+            // 2xx success
             static const Status ok;
             static const Status created;
+            static const Status accepted;
+            static const Status nonAuthoritativeInformation;
+            static const Status noContent;
+            static const Status resetContent;
+            static const Status partialContent;
+            static const Status multiStatus;
+            static const Status alreadyReported;
+            static const Status imUsed;
 
+            // 3xx redirection
+            static const Status multipleChoices;
+            static const Status movedPermanently;
+            static const Status found;
+            static const Status seeOther;
+            static const Status notModified;
+            static const Status useProxy;
+            static const Status switchProxy;
+            static const Status temporaryRedirect;
+            static const Status permanentRedirect;
+
+            // 4xx Client errors
+            static const Status badRequest;
+            static const Status unauthorized;
+            static const Status paymentRequired;
+            static const Status forbidden;
             static const Status notFound;
+            static const Status methodNotAllowed;
+            static const Status notAcceptable;
+            static const Status proxyAuthenticationRequired;
+            static const Status requestTimeout;
+            static const Status conflict;
+            static const Status gone;
+            static const Status lengthRequired;
+            static const Status preconditionFailed;
+            static const Status payloadTooLarge;
+            static const Status uriTooLong;
             static const Status unsupportedMediaType;
+            static const Status rangeNotSatisfiable;
+            static const Status expectationFailed;
+            static const Status imATeaPot;
+            static const Status misdirectedRequest;
+            static const Status unprocessableEntity;
+            static const Status locked;
+            static const Status failedDependency;
+            static const Status tooEarly;
+            static const Status upgradeRequired;
+            static const Status preconditionRequired;
+            static const Status tooManyRequests;
+            static const Status requestHeaderFieldsTooLarge;
+            static const Status unavailableForLegalReasons;
+
+            // 5xx Server errors
+            static const Status internalServerError;
+            static const Status notImplemented;
+            static const Status badGateway;
+            static const Status serviceUnavailable;
+            static const Status gatewayTimeout;
+            static const Status httpVersionNotSupported;
+            static const Status variantAlsoNegotiates;
+            static const Status insufficientStorage;
+            static const Status loopDetected;
+            static const Status notExtended;
+            static const Status networkAuthenticationRequired;
+
         };
 
     protected:
--- src/Response.cpp
+++ src/Response.cpp
@@ -2,10 +2,71 @@
 
 using namespace ARo::Http;
 
+
+// 2xx success
 const Response::Status Response::Status::ok{200, "OK"};
 const Response::Status Response::Status::created{201, "Created"};
+const Response::Status Response::Status::accepted{202, "Accepted"};
+const Response::Status Response::Status::nonAuthoritativeInformation{203, "Non-authoritative information"};
+const Response::Status Response::Status::noContent{204, "No content"};
+const Response::Status Response::Status::resetContent{205, "Reset content"};
+const Response::Status Response::Status::partialContent{206, "Partial content"};
+const Response::Status Response::Status::multiStatus{207, "Multi-status"};
+const Response::Status Response::Status::alreadyReported{208, "Already reported"};
+const Response::Status Response::Status::imUsed{226, "IM Used"};
+
+// 3xx redirection
+const Response::Status Response::Status::multipleChoices{300, "Multiple choices"};
+const Response::Status Response::Status::movedPermanently{301, "Moved permanently"};
+const Response::Status Response::Status::found{302, "Found"};
+const Response::Status Response::Status::seeOther{303, "See other"};
+const Response::Status Response::Status::notModified{304, "Not modified"};
+const Response::Status Response::Status::useProxy{305, "Use proxy"};
+const Response::Status Response::Status::switchProxy{306, "Switch proxy"};
+const Response::Status Response::Status::temporaryRedirect{307, "Temporary redirect"};
+const Response::Status Response::Status::permanentRedirect{308, "Permanent redirect"};
+
+const Response::Status Response::Status::badRequest{400, "Bad request"};
+const Response::Status Response::Status::unauthorized{401, "Unauthorized"};
+const Response::Status Response::Status::paymentRequired{402, "Payment required"};
+const Response::Status Response::Status::forbidden{403, "Forbidden"};
 const Response::Status Response::Status::notFound{404, "Not found"};
+const Response::Status Response::Status::methodNotAllowed{405, "Method not allowed"};
+const Response::Status Response::Status::notAcceptable{406, "Not acceptable"};
+const Response::Status Response::Status::proxyAuthenticationRequired{407, "Proxy authentication required"};
+const Response::Status Response::Status::requestTimeout{408, "Request timeout"};
+const Response::Status Response::Status::conflict{409, "Conflict"};
+const Response::Status Response::Status::gone{410, "Gone"};
+const Response::Status Response::Status::lengthRequired{411, "Length required"};
+const Response::Status Response::Status::preconditionFailed{412, "Precondition failed"};
+const Response::Status Response::Status::payloadTooLarge{413, "Payload too large"};
+const Response::Status Response::Status::uriTooLong{414, "URI too long"};
 const Response::Status Response::Status::unsupportedMediaType{415, "Unsupported media type"};
+const Response::Status Response::Status::rangeNotSatisfiable{416, "Range not satisfiable"};
+const Response::Status Response::Status::expectationFailed{417, "Expectation failed"};
+const Response::Status Response::Status::imATeaPot{418, "I'm a teapot"};
+const Response::Status Response::Status::misdirectedRequest{421, "Misdirected request"};
+const Response::Status Response::Status::unprocessableEntity{422, "Unprocessable entity"};
+const Response::Status Response::Status::locked{423, "Locked"};
+const Response::Status Response::Status::failedDependency{424, "Failed dependency"};
+const Response::Status Response::Status::tooEarly{425, "Too early"};
+const Response::Status Response::Status::upgradeRequired{426, "Upgrade required"};
+const Response::Status Response::Status::preconditionRequired{428, "Precondition required"};
+const Response::Status Response::Status::tooManyRequests{429, "Too many requests"};
+const Response::Status Response::Status::requestHeaderFieldsTooLarge{431, "Request header fields too large"};
+const Response::Status Response::Status::unavailableForLegalReasons{451, "Unavailable for legal reasons"};
+
+const Response::Status Response::Status::internalServerError{500, "Internal server error"};
+const Response::Status Response::Status::notImplemented{501, "Not implemented"};
+const Response::Status Response::Status::badGateway{502, "Bad gateway"};
+const Response::Status Response::Status::serviceUnavailable{503, "Service unavailable"};
+const Response::Status Response::Status::gatewayTimeout{504, "Gateway timeout"};
+const Response::Status Response::Status::httpVersionNotSupported{505, "HTTP version not supported"};
+const Response::Status Response::Status::variantAlsoNegotiates{506, "Variant also negotiates"};
+const Response::Status Response::Status::insufficientStorage{507, "Insufficient storage"};
+const Response::Status Response::Status::loopDetected{508, "Loop detected"};
+const Response::Status Response::Status::notExtended{510, "Not extended"};
+const Response::Status Response::Status::networkAuthenticationRequired{511, "Network authentication required"};
 
 Response::~Response() = default;
 
